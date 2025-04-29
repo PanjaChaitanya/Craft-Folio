67e5e25d06932ee4a705fd692e97635469d44af7
PanjaChaitanya <panjachaitanya23@gmail.com>
2025-04-29 10:14:31 +0530

    Added Static Hero content

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,78 @@
+import { FaLinkedin, FaSquareXTwitter } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import { Link } from "react-router-dom";
+
+const Hero = () => {
+	return (
+		<div
+			id="home"
+			className="first-letter:relative h-screen bg-white flex items-center justify-center px-4 md:px-8 lg:px-16"
+		>
+			<div className="flex flex-col-reverse lg:flex-row items-center justify-between gap-12 min-h-screen mt-20 sm:mt-0">
+				{/* Left Content */}
+				<div className="flex-1 space-y-6 ml-0 sm:ml-16">
+					<div className="mb-5">
+						<h2 className="text-md uppercase tracking-wider text-gray-600">
+							Hello, My name is
+						</h2>
+						<div className="w-14 h-[2px] bg-gray-500 mt-2" />
+					</div>
+					<div>
+						<h1 className="text-5xl md:text-6xl font-semibold mt-2">
+							Chaitanya Panja
+						</h1>
+						<h3 className="text-2xl md:text-3xl text-gray-600 mt-2">
+							Frontend Developer
+						</h3>
+					</div>
+
+					<div className="space-y-2 pt-1">
+						<div className="flex items-center space-x-2">
+							<p className="text-lg text-gray-600 max-w-[500px]">
+								As a passionate frontend developer, I specialize in turning
+								visionary concepts into stunning, user-friendly web
+								applications. Let&#39;s collaborate to bring your ideas to life and
+								create solutions that make a difference in the world.
+							</p>
+						</div>
+
+						<div className="flex items-center space-x-4 pt-4">
+							<Link to="https://github.com/Moyo-Made">
+								<FaGithub size={20} />
+							</Link>
+
+							<Link to="https://www.linkedin.com/in/moyomade-adegbite/">
+								<FaLinkedin size={20} />
+							</Link>
+
+							<Link to="https://x.com/moyomadee7">
+								<FaSquareXTwitter size={20} />
+							</Link>
+
+							<Link href="mailto:adegbitemoyomade2004@gmail.com">
+								<IoMdMail size={20} />
+							</Link>
+						</div>
+					</div>
+				</div>
+
+				{/* Right Content - Image */}
+				<div className="flex justify-center items-center">
+					<div className="aspect-square rounded-full overflow-hidden bg-gray-900 mt-0 w-[280px] sm:w-[300px] md:mt-20 lg:mt-0 md:w-[350px] lg:w-[450px]">
+						<img
+							src="/images/profile.PNG"
+							alt="Profile"
+							width={1920}
+							height={1920}
+							priority
+							className="w-full h-full object-cover"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from 'react-helmet';
 import Projects from '../components/Projects'
 import Skills from '../components/Skills';
+import Hero from '../components/Hero';
 
 export default function Home() {
   return (
@@ -16,10 +17,7 @@ export default function Home() {
       <div className="p-8">
         {/* Intro / Hero Section */}
         <section className="mb-12 text-center">
-          <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
-          <p className="text-lg text-gray-600">
-            Explore my projects and the skills I’ve mastered along the way.
-          </p>
+          <Hero/>
         </section>
 
         {/* Projects Section */}
